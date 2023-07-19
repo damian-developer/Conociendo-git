c030d45f9ac46079bf1533626cf302ade42006da
Damián Canónica <damiancanonica90@gmail.com>
2023-07-18 21:39:31 -0300

    errores xD

--- artista.py
+++ artista.py
@@ -1,7 +1,3 @@
-class Artista:
-    def __init__(self, nombre):
-        self.nombre = nombre
-        self.canciones = []
 
     def lanzar_cancion(self, cancion):
         self.canciones.append(cancion)
--- cancion.py
+++
@@ -1,8 +0,0 @@
-class Cancion:
-    def __init__(self, nombre, duracion, artista):
-        """
-        Inicializa una nueva canción con un nombre, duración y artista.
-        """
-        self.nombre = nombre
-        self.duracion = duracion
-        self.artista = artista
